f5b0d582d427b327299402434c8a6e7c013bedc9
Scott Anderson <yankeesoccernut@gmail.com>
2017-11-12 09:44:12 -0500

    revised structure starting to take shape

--- meterMaid/meterMaidRewrite.js
+++ meterMaid/meterMaidRewrite.js
@@ -1,29 +1,22 @@
 // meterMaid is designed to poll Honeywell wifi thermostats for current settings and readings.  It utilizes an older API to access the thermostats and their settings and readings in a database for later use.
 
 // program structure....
-// 1.  get the userid and password for logging into the Honeywell platform.
-// 2.  use the API to request all locations and current reading for each location.  The current Honeywell API returns XML.
-// 3.  parse through the XML and construct valid database records for insertion into the target database tables.
-// 4.  insert the validated rows into the target tables.
-// 5.  insert an entry in the activity log to record the polling activity.
+// 1.  get the userid and password for logging into the Honeywell platform.  TODO:  implement mulit-user via the users credentials that we capture on the front-end.
+// 2.  use the API to request all locations and current reading for each location.  The current Honeywell API returns XML so it gets transformed to JSON.
+// 3.  Use the transformed JSON to process the poll results
 
-var mysql = require('mysql');
 var moment = require ('moment');
 var config = require('../config');
 var createActivity = require('../utility/createActivity');
 
 var sessionID = '';
 var pollResults = [];
-var dbPromises = [];  // // TODO: PROMISES
-
 
 console.log("Start of processing....");
 getHoneywellSessionId()
 .then((sessionID) => {
-  console.log(`Session ID: ${sessionID}`);
   pollHoneywellUserData()
   .then((pollResults) => {
-    console.log(pollResults);
     logoffHoneywell();
     processHoneywellPoll(pollResults);
   });
@@ -31,6 +24,36 @@ getHoneywellSessionId()
 
 console.log("!!!!!Next Lines of Main Code Are Running!!!!!")
 // console.log (pollResults);
+
+function processHoneywellPoll(poll){
+  // Poll contains Location information for each site the user has registered with Honeywell.  There is also Thermostat and Reading data with each Poll.
+  // Iterate through Locations, Thermostat, and Readings...
+  // With each iteration:
+  // 1.  Check to see if there are LocationHours in our DB.  These are used to determine if the Reading occurred during business hours or not.  This is a critical boolean value for the overall system.
+  // 2.  If there are no LocationHours, create an Activity to remind the user that they need to define them in order to get value from the system.
+  // 3.  Format a complete Reading record and INSERT it into our DB.
+
+  // We'll open and close the DB from this function...
+  var mysql = require('mysql');
+  var dbInsertPromises = [];  //TODO: PROMISES
+
+
+  poll.map((site) => {
+    checkLocationHours(site.LocationData.LocationID)
+    .then((hasLocationHours) => {
+      console.log(hasLocationHours);
+    });
+  }); // poll.map
+
+}; // processHoneywellPoll
+function checkLocationHours(locationID){
+  // Check the DB to see if the user has established LocationHours.
+  console.log(`locationID: ${locationID}`);
+
+  return( new Promise (function(resolve, reject) {
+    resolve(true);
+  }));
+}; // checkLocationHours
 
 function getHoneywellSessionId(){
 // Get user id and password.....// TODO: encrypt/decrypt for security
