8b2d4d149ec6de8d1a55d2cd2959e95a44951d12
Michael McFarland <gardengrove422@gmail.com>
2017-11-13 14:32:43 -0500

    6th

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ var config = require('../config');
 
 var bcrypt = require('bcrypt-nodejs');
 
+var connection = mysql.createConnection(config.db);
+connection.connect((error)=>{
+	if (error){
+		throw error;
+	}
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -35,9 +42,42 @@ router.get('/locations', function(req, res, next) {
 
 router.post('/loginProcess',(req,res,next)=>{
   // Check to see if email/pass exists in database
+  console.log(req.body);
+  var email = req.body.email;
+  var password = req.body.password; // English version from user
+  // write a query to check if the user is the DB
+  var selectQuery = `SELECT * FROM users WHERE email = ?;`;
+  connection.query(selectQuery,[email],(err,results)=>{
+      if(err){
+          console.log(err);
+      }else{
+          if(results.length == 0){
+              // this user isn't in the databse. We dont care what pass they gave us.
+              res.redirect('/?msg=notValidEmail');
+              // show not valid input modal
+          }else{
+              // our select query found something! Check the pass...
+              // call compareSync
+              var passwordsMatch = bcrypt.compareSync(password,results[0].password)
+              if(passwordsMatch){
+                  var row = results[0];
+                  // user in db, password is legit. Log them in.
+                  req.session.uid = row.id;
+                  req.session.email = row.email;
+                  req.session.loggedIn = true;
+                  console.log(req.session.uid)
+                  res.redirect('users/usersProfile');
+              }else{
+                  // user in db, but password is bad. Send them back to login
+                  res.redirect('/login?msg=badPass');
+                  // show not valid input modal
+              }
+          }
+      }
+  });
 
-	console.log('im in INDEX')
-  res.send("Need to implement the authentication for entered email/pass");
+
+  // res.send("Need to implement the authentication for entered email/pass");
 })
 
 // router.get('/', function(req, res, next) {
