fd28af51462794eee78a5e3ba1aa27a04e38300b
AHowardC <achsvgusaeng@yahoo.com>
2017-11-13 14:33:57 -0500

    hit the basic routes

--- public/javascripts/usersProfile.js
+++ public/javascripts/usersProfile.js
@@ -2,18 +2,25 @@ $(document).ready(()=>{
 
 // when user hits submit validate credentials 
 // from honeywell 
-// 0 they will type in their honeywell credentials
-// 1 attempt to login with their credentials
-// 2 if (we get a session id){ 
+// 0. they will type in their honeywell credentials
+// 1. attempt to login with their credentials
+// 2. if (we get a session id){ 
 // we will store them with bcrypt} 
-// 3 (else)otherwise {tell them not valid try again}
-// 4 the user only gets 3 attempts 
+// 3. (else)otherwise {tell them not valid try again}
+// 4. the user only gets 3 attempts 
+// 5. var attempts = 0
+//   if(attempts > 3){
+//     res.send('You are locked out. Too many attempts.')
+//   }
   
 
-  $("#validateHoneywell").click(function() {
-    console.log( "#validateHoneywell.click" );
-    
-  });
+  // $("#validateHoneywell").click(function() {
+  //   console.log( "#validateHoneywell.click" );
+  //   $.post("/api/validateHoneywell", function({email}){
+
+  //   });//$.post("/api/validateHoneywell"
+  
+  // });
 
 
 });  // document ready
