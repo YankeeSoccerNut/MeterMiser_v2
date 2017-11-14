03b65f43e913a67edfdb3643abf2d9bb97bc5d64
Mikayla Kelhofer <mikaylakelhofer@gmail.com>
2017-11-14 13:42:31 -0500

    add functionality for normalized view

--- public/javascripts/performance-data.js
+++ public/javascripts/performance-data.js
@@ -54,7 +54,7 @@ $(document).ready(()=>{
 	
 
 	// var settingRow = function(d) {return d.status};
-	var settingRowScale = d3.scaleBand().range([0,height]).paddingInner(0.25);
+	var settingRowScale = d3.scaleBand().paddingInner(0.25);
 
 	// var storeHoursRow = function(d) {return d.}
 	var storeHoursRowScale = d3.scaleBand().range([0, height]).paddingInner(0.2);
@@ -66,25 +66,31 @@ $(document).ready(()=>{
 
 	// Define Domains
 	xScale.domain(['East Cobb', 'West Cobb', 'Roswell']);
-	settingRowScale.domain(['Off', 'Scheduled', 'Hold - Temporary', 'Hold - Permanent',]);
-	storeHoursRowScale.domain(['Store Open','Store Closed']);
+	settingRowScale.domain(['Off', 'Scheduled', 'Hold - Temporary', 'Hold - Permanent']);
+	storeHoursRowScale.domain(['Closed','Open']);
+	settingRowScale.range([0,storeHoursRowScale.bandwidth()]);
 
 	// Define the axes
 	var xAxis = d3.axisBottom(xScale).ticks(3);
 	var yAxis = d3.axisLeft(settingRowScale);
 
 	var ySplitScale = d3.scaleLinear().range([settingRowScale.bandwidth(),0]);;
-	var yStackScale = d3.scaleLinear().range([height,0]);
+	var yStackScale = d3.scaleLinear().range([storeHoursRowScale.bandwidth(),0]);
 
 	// Get the data
 	d3.json('/api/Readings', function(error,data){
 		console.log(data);
-		var dataFormated = formatJSON(data);
+		var dataInRange = data.filter(function(d){
+			return Date.parse(d.created) > Date.parse(start) && Date.parse(d.created) < Date.parse(end)
+		})
+		var dataFormated = formatJSON(dataInRange)
 		console.log(dataFormated)
-		console.log(nestedHours(dataFormated))
-		var dataBySetting = nestedSetting(dataFormated);
-		console.log(dataBySetting)
-		console.log(d3.max(dataBySetting, function(d){return d3.max(d, function(v){return v.value})}))
+		var dataByHours = nestedHours(dataFormated);
+		console.log(dataByHours)
+		var locationTimeSums =totalTimeByLocation(dataByHours)
+		console.log(locationTimeSums)
+		var dataByHoursPercents = percentValues(dataByHours)
+		// console.log(d3.max(dataBySetting, function(d){return d3.max(d, function(v){return v.value})}))
 
 
 		var startingTimeStamp = Date.parse(start);
@@ -100,7 +106,7 @@ $(document).ready(()=>{
 
 		ySplitScale.domain([0,(endingTimeStamp-startingTimeStamp)])
 		
-		yStackScale.domain([0,(endingTimeStamp-startingTimeStamp)])
+		yStackScale.domain([0,1])
 		
 
 		// yStackScale.domain([0,d3.max(dataByLocation, function(d) {return d.value})])
@@ -111,15 +117,25 @@ $(document).ready(()=>{
         .call(xAxis);
 
 
-		var gRow = svg.append('g').attr('class', 'row')
-            .selectAll('.row').data(dataBySetting)
+		var oRow = svg.append('g').attr('class', 'row')
+            .selectAll('.row').data(dataByHoursPercents)
+        .enter().append('g')
+            .attr('class', 'hours-group')
+            .attr("transform", function(d) {  return "translate(0," + storeHoursRowScale(d.key) + ")"; });
+
+        oRow.append('text').attr('class', 'title')
+	        .attr('dx', '-0.34em')
+	        .text(function(d) {return d.key});
+
+	    var gRow = oRow.append('g').attr('class', 'row')
+            .selectAll('.row').data(function(d) {return d.values;})
         .enter().append('g')
             .attr('class', 'group')
             .attr('fill', function(d) { return colorScale(d); });
 
-        gRow.append('text').attr('class', 'title')
-	        .attr('dx', '-0.34em')
-	        .text(function(d) {return d.key});
+        // gRow.append('text').attr('class', 'title')
+	       //  .attr('dx', '-0.34em')
+	       //  .text(function(d) {return d.key});
 
          var bars = gRow.append('g').attr('class', 'bars');
 
@@ -138,9 +154,9 @@ $(document).ready(()=>{
         	.enter().append('rect')
         		.attr('class', 'bar bar-overlying')
 	            .attr('x', function(d) {return xScale(d.key);})
-	            .attr('y', function(d) {return yStackScale(d.stackSum + d.value);})
+	            .attr('y', function(d) {return yStackScale(d.stackSumPercByLocPerH + d.pByLocPerH);})
 	            .attr('width', xScale.bandwidth())
-	            .attr('height', function(d) { return yStackScale(0) - yStackScale(d.value);})
+	            .attr('height', function(d) { return yStackScale(0) - yStackScale(d.pByLocPerH);})
 
 
    
@@ -184,12 +200,16 @@ $(document).ready(()=>{
 			dataFormated[i].location = LocationName(d.thermostatId);
 			dataFormated[i].dateTimeInfo = new DateTime(d.created);
 			dataFormated[i].status = determineStatus(d.statusHeat, d.systemSwitchPos);
-			dataFormated[i].operatingHours = d.operatingHoursFlag;
 			if(i>2){
 				dataFormated[i-3].endTimeInfo = new DateTime(d.created);
 				if(i>=rawData.length - 3){
 					dataFormated[i].endTimeInfo =new DateTime(Date.now());
 				}
+			}
+			if(d.operatingHoursFlag == 0){
+				dataFormated[i].operatingHours = 'Closed';
+			}else{
+				dataFormated[i].operatingHours = 'Open';
 			}
 
 		})
@@ -266,13 +286,6 @@ $(document).ready(()=>{
 		return nestedData;
 	}
 
-	function maxNestedSetting(nestedSettingData){
-		for(let i = 0; i<nestedSettingData.length; i++){
-
-
-		}
-		
-	}
 
 	function nestedHours(dataFormated){
 		var nestedData = d3.nest()
@@ -289,21 +302,88 @@ $(document).ready(()=>{
 			var wcSum = 0;
 			var rSum = 0;
 			for(let j = 0; j < nestedData[i].values.length; j++){
+				var settingSum = 0;
 				for(let k = 0; k < nestedData[i].values[j].values.length; k++)
-				if(nestedData[i].values[j].values[k].key == 'East Cobb'){
-					nestedData[i].values[j].values[k].stackSum = ecSum;
-					ecSum += nestedData[i].values[j].values[k].value;
-				}else if(nestedData[i].values[j].values[k].key == 'West Cobb'){
-					nestedData[i].values[j].values[k].stackSum = wcSum;
-					wcSum += nestedData[i].values[j].values[k].value;
-				}else if(nestedData[i].values[j].values[k].key == 'Roswell'){
-					nestedData[i].values[j].values[k].stackSum = rSum;
-					rSum += nestedData[i].values[j].values[k].value;
-				}
+					if(nestedData[i].values[j].values[k].key == 'East Cobb'){
+						nestedData[i].values[j].values[k].stackSum = ecSum;
+						ecSum += nestedData[i].values[j].values[k].value;
+						settingSum += nestedData[i].values[j].values[k].value;
+					}else if(nestedData[i].values[j].values[k].key == 'West Cobb'){
+						nestedData[i].values[j].values[k].stackSum = wcSum;
+						wcSum += nestedData[i].values[j].values[k].value;
+						settingSum += nestedData[i].values[j].values[k].value;
+					}else if(nestedData[i].values[j].values[k].key == 'Roswell'){
+						nestedData[i].values[j].values[k].stackSum = rSum;
+						rSum += nestedData[i].values[j].values[k].value;
+						settingSum += nestedData[i].values[j].values[k].value;
+					}
+					nestedData[i].values[j].settingSum = settingSum;
 			}
+			
+			nestedData[i].hoursSum = ecSum+wcSum+rSum;
+			nestedData[i].ecSum = ecSum;
+			nestedData[i].wcSum = wcSum;
+			nestedData[i].rSum = rSum;
+
 
 		}
 		return nestedData;
+	}
+
+	function totalTimeByLocation(nestedHoursData){
+		var timeByLocal = {
+			ec: [0],
+			wc: [0],
+			r: [0],
+			total: [0]
+		};
+		for(let i = 0; i < nestedHoursData.length; i++){
+			timeByLocal.ec[i+1] = nestedHoursData[i].ecSum;
+			timeByLocal.ec[0] += nestedHoursData[i].ecSum
+			timeByLocal.wc[i+1] = nestedHoursData[i].wcSum;
+			timeByLocal.wc[0] += nestedHoursData[i].wcSum;
+			timeByLocal.r[i+1] = nestedHoursData[i].rSum;
+			timeByLocal.r[0] +=nestedHoursData[i].rSum;
+			timeByLocal.total[i+1] = timeByLocal.ec[i+1]+timeByLocal.wc[i+1]+timeByLocal.r[i+1];
+			timeByLocal.total[0] += timeByLocal.ec[i+1]+timeByLocal.wc[i+1]+timeByLocal.r[i+1];
+		}
+		return timeByLocal
+	}
+
+	function percentValues(nestedHoursData){
+		var timeByLocal = totalTimeByLocation(nestedHoursData);
+		var nestedData = nestedHoursData;
+
+		for(let i = 0; i < nestedData.length; i++){
+			for(let j = 0; j < nestedData[i].values.length; j++){
+				for(let k = 0; k < nestedData[i].values[j].values.length; k++)
+					if(nestedData[i].values[j].values[k].key == 'East Cobb'){
+						nestedData[i].values[j].values[k].pByLoc = nestedData[i].values[j].values[k].value/timeByLocal.ec[0];
+						nestedData[i].values[j].values[k].pByLocPerH = nestedData[i].values[j].values[k].value/timeByLocal.ec[i+1];
+						nestedData[i].values[j].values[k].stackSumPercByLoc = nestedData[i].values[j].values[k].stackSum/timeByLocal.ec[0];
+						nestedData[i].values[j].values[k].stackSumPercByLocPerH = nestedData[i].values[j].values[k].stackSum/timeByLocal.ec[i+1];
+					}else if(nestedData[i].values[j].values[k].key == 'West Cobb'){
+						nestedData[i].values[j].values[k].pByLoc = nestedData[i].values[j].values[k].value/timeByLocal.wc[0];
+						nestedData[i].values[j].values[k].pByLocPerH = nestedData[i].values[j].values[k].value/timeByLocal.wc[i+1];
+						nestedData[i].values[j].values[k].stackSumPercByLoc = nestedData[i].values[j].values[k].stackSum/timeByLocal.wc[0];
+						nestedData[i].values[j].values[k].stackSumPercByLocPerH = nestedData[i].values[j].values[k].stackSum/timeByLocal.wc[i+1]
+					}else if(nestedData[i].values[j].values[k].key == 'Roswell'){
+						nestedData[i].values[j].values[k].pByLoc = nestedData[i].values[j].values[k].value/timeByLocal.r[0];
+						nestedData[i].values[j].values[k].pByLocPerH = nestedData[i].values[j].values[k].value/timeByLocal.r[i+1];
+						nestedData[i].values[j].values[k].stackSumPercByLoc = nestedData[i].values[j].values[k].stackSum/timeByLocal.r[0];
+						nestedData[i].values[j].values[k].stackSumPercByLocPerH = nestedData[i].values[j].values[k].stackSum/timeByLocal.r[i+1]
+					}
+					nestedData[i].values[j].settingP = nestedData[i].values[j].settingSum/timeByLocal.total[0];
+					nestedData[i].values[j].settingPperH = nestedData[i].values[j].settingSum/timeByLocal.total[i+1];
+			}
+			
+			nestedData[i].hoursP = nestedData[i].hoursSum/timeByLocal.total[0];
+
+
+		}
+		return nestedData;
+
+
 	}
 
 
@@ -320,38 +400,6 @@ $(document).ready(()=>{
 		this.date = this.dateComplete.getUTCDate();
 		this.hour = this.dateComplete.getUTCHours();
 		this.minute = this.dateComplete.getUTCMinutes();
-		this.duringOperationHours = compareToStoreHours(this.hour);
-	}
-
-	function compareToStoreHours(hour){
-		var storeHours = {
-			open: 14, //9am (shifted 5 for UTC)
-			close: 23  //6pm (shifted 5 for UTC)
-		}
-		if(hour >= storeHours.open && hour < storeHours.close){
-			return true;
-		}else{
-			return false;
-		}
-	}
-
-	function filterByTimeframe(data, timeframe){
-		var dataByTimeframe = [];
-		var mostRecent = d3.max(dataFormated, function(d){return d.dateTimeInfo.timeStamp});
-		var timeframe;
-		if(domain == 'oneDay'){
-			timeframe = 24*60*60*1000;
-		}else if(domain == 'oneWeek'){
-			timeframe = 24*60*60*1000*7;
-		}else{//month
-			timeframe = 24*60*60*1000*30;
-		}
-		data.forEach(function(d){
-			if(d.dateTimeInfo.timeStamp >= mostRecent - timeframe){
-				dataByTimeframe.push(d);
-			}
-		})
-		return dataByTimeframe;		
 	}
 
 	
@@ -381,77 +429,160 @@ $(document).ready(()=>{
     }
 
 	function updateData(start,end){
-		d3.select("#table-container").select("#history-table_wrapper").remove();
-		// Get the data again
 		d3.json('/api/Readings', function(error,data){
 			console.log(data);
-			var dataFormated = formatJSON(data);
-			console.log(dataFormated);
-
 			var dataInRange = data.filter(function(d){
 				return Date.parse(d.created) > Date.parse(start) && Date.parse(d.created) < Date.parse(end)
 			})
-			var dataForTable = formatJSON(dataInRange)
-			console.log(dataForTable)
-
-			// Reset Domains
-			x.domain([start, end]);
-			// console.log(new Date(mostRecent-timeframe));
-			y.domain(['East Cobb', 'West Cobb', 'Roswell']);
-
-			 // Select the section we want to apply our changes to
-		    var svg = d3.select("#graph-container");
-
-		    // Data join
-		    var rects = svg.selectAll('.rect')
-		    	.data(dataFormated);
-
-		    // Make the changes
-		    //remove unneeded rects
-		    rects.exit().remove();
-		    //add any new rects
-		    rects.enter().append('rect')
-		    	.attr("class", "rect")
-		    	.style("fill", function(d) {return colorPicker(d.status)})
-			//update all rects to new position
-			rects.transition()
-				.duration(750)
-				.attr("x", function(d) {return x(d.dateTimeInfo.dateComplete); })
-				.attr("y", function(d) {return y(d.location); })
-				.attr("width", function(d) {return x(d.endTimeInfo.dateComplete) - x(d.dateTimeInfo.dateComplete); })
-				.attr("height", y.bandwidth())
-				// .style("opacity", )
+			var dataFormated = formatJSON(dataInRange)
+			console.log(dataFormated)
+			var dataByHours = nestedHours(dataFormated);
+			console.log(dataByHours)
+			// console.log(d3.max(dataBySetting, function(d){return d3.max(d, function(v){return v.value})}))
 
 
-		    svg.select(".x.axis").transition()
-		    	.duration(750) // change the x axis
-		        .call(xAxis)
-		         .selectAll("text")	
-	            .style("text-anchor", "end")
-	            .attr("dx", "-.8em")
-	            .attr("dy", ".15em")
-	            .attr("transform", function(d) {
-	                return "rotate(-60)" 
-	                });
-		    svg.select(".y.axis").transition() // change the y axis
-		        .duration(750)
-		        .call(yAxis)
-		        .selectAll("text")	
-	            .style("text-anchor", "middle")
-	            .attr("y", -10)
-	            .attr("dx", "5px")
-	            .attr("dy", "1px")
-	            .attr("transform", function(d) {
-	                return "rotate(-90)" 
-	                });
+			var startingTimeStamp = Date.parse(start);
+			var endingTimeStamp = Date.parse(end);
+			if(startingTimeStamp < dataFormated[0].dateTimeInfo.timeStamp){
+				startingTimeStamp = dataFormated[0].dateTimeInfo.timeStamp
+			}
+			if(endingTimeStamp > dataFormated[dataFormated.length-1].endTimeInfo.timeStamp){
+				endingTimeStamp = dataFormated[dataFormated.length-1].endTimeInfo.timeStamp
+			}
+			console.log(endingTimeStamp-startingTimeStamp)
+			var totalMilliseconds = endingTimeStamp-startingTimeStamp;
 
-             //render the table
+			ySplitScale.domain([0,(endingTimeStamp-startingTimeStamp)]);
+			
+			yStackScale.domain([0,(endingTimeStamp-startingTimeStamp)]);
+			
 
-		    var dataTable = tabulate(dataForTable, ["Date", "Location", "Status"]);
-		    $('#history-table').dataTable();
+			// yStackScale.domain([0,d3.max(dataByLocation, function(d) {return d.value})])
 
-	    });
+			svg.append("g")
+	        .attr("class", "x axis")
+	        .attr("transform", "translate(0," + height + ")")
+	        .call(xAxis);
+
+
+			var oRow = svg.append('g').attr('class', 'row')
+	            .selectAll('.row').data(dataByHours)
+	        .enter().append('g')
+	            .attr('class', 'hours-group')
+	            .attr("transform", function(d) {  return "translate(0," + storeHoursRowScale(d.key) + ")"; });
+
+	        oRow.append('text').attr('class', 'title')
+		        .attr('dx', '-0.34em')
+		        .text(function(d) {return d.key});
+
+		    var gRow = oRow.append('g').attr('class', 'row')
+	            .selectAll('.row').data(function(d) {return d.values;})
+	        .enter().append('g')
+	            .attr('class', 'group')
+	            .attr('fill', function(d) { return colorScale(d); });
+
+	        // gRow.append('text').attr('class', 'title')
+		       //  .attr('dx', '-0.34em')
+		       //  .text(function(d) {return d.key});
+
+	         var bars = gRow.append('g').attr('class', 'bars');
+
+	        bars.selectAll(".bar-underlying").data(function(d) {return d.values;})
+	        	.enter().append('rect')
+	        		.attr('class', 'bar bar-underlying')
+		            .attr('x', function(d) {return xScale(d.key);})
+		            .attr('y', 0)
+		            .attr('width', xScale.bandwidth())
+		            .attr('height', function(d) { return yStackScale.range()[1];})
+		            .style('fill', '#fff');
+
+
+
+	        bars.selectAll(".bar-overlying").data(function(d) {return d.values;})
+	        	.enter().append('rect')
+	        		.attr('class', 'bar bar-overlying')
+		            .attr('x', function(d) {return xScale(d.key);})
+		            .attr('y', function(d) {return yStackScale(d.stackSum + d.value);})
+		            .attr('width', xScale.bandwidth())
+		            .attr('height', function(d) { return yStackScale(0) - yStackScale(d.value);})
+
+
+	   
+		         
+
+		 //            
+
+			 //check selected element in div with property 'check'
+		    d3.selectAll("input")
+		      .on("change", change);
+		    
+		    function change() {
+			    if (this.value === "stacked") transitionStacked();
+			    else transitionMultiples ();
+		    }
+
+		    d3.select("input[value=\"stacked\"]")
+		    .property("checked", true);
+	});
 	}
+
+	function tabulate(data, columns) {
+		formatTimeTable = d3.timeFormat('%a %b %-d %-I:%M %p');
+	    var table = d3.select("#table-container").append("table")
+	            .attr("id","performance-table")
+	            .attr("class","table")
+	            .attr("class","table-striped")
+	            .attr("class","table-bordered")
+	            .attr("cellspacing", "0")
+	            .attr("width",'100%'),
+	        thead = table.append("thead"),
+	        tbody = table.append("tbody");
+
+	    // append the header row
+	    thead.append("tr")
+	        .selectAll("th")
+	        .data(columns)
+	        .enter()
+	        .append("th")
+	            .text(function(column) { return column; });
+
+	    // create a row for each object in the data
+	    var rows = tbody.selectAll("tr")
+	        .data(data)
+	        .enter()
+	        .append("tr");
+
+	    // create a cell in each row for each column
+	    var cells = rows.selectAll("td")
+	        .data(function(d) {
+	            return columns.map(function(column) {
+	            	if(column == "Date"){
+	            		return {column: column, value: formatTimeTable(d.dateTimeInfo.dateComplete)}
+	            	}else if(column == "Location"){
+	            		return {column: column, value: d.location}
+	            	}else if(column == "Status"){
+	            		return {column: column, value: d.status};
+	            	}else{
+	            		icons = ''
+	            			if(d.triggers.connection != ''){
+	            				icons += `<i class="material-icons">location_off</i>`
+	            			}
+	            			if(d.triggers.permHold){
+	            				icons += `<i class="material-icons">priority_high</i>`
+	            			}
+
+	            		return {column: column, value: icons};
+	            	}
+	            });
+	        })
+	        .enter()
+	        .append("td")
+	        .attr("style", "color: black") // sets the font style
+	            .html(function(d) { return d.value; });
+	    
+	    return table;
+	}
+
 
 
 
